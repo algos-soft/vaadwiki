af30f335c3cde05b4f4f9c13cf274e15d54225a4
algos-soft <gac@algos.it>
2018-11-06 13:27:28 +0100

    ...

--- src/main/java/it/algos/vaadflow/wizard/enumeration/Progetto.java
+++ src/main/java/it/algos/vaadflow/wizard/enumeration/Progetto.java
@@ -17,7 +17,7 @@ public enum Progetto {
 
     vaadin("vaadflow", "vaadflow", "vaadtest", "Flow"),
     test("vaadflow", "vaadtest", "vaadtest", "Test"),
-    bio("vaadwiki", "vaadbio", "vaadbio", "Bio"),
+    bio("vaadwiki", "vaadwiki", "vaadwiki", "Wiki"),
     wam("vaadwam", "vaadwam", "vaadwam", "Wam"),
     ;
 
