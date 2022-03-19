54e75a3d48bc02a9162695c6c22d004b2deb28db
algos-soft <gac@algos.it>
2022-03-19 10:26:38 +0100

    ...package

--- src/main/java/it/algos/vaadwiki/application/WikiBoot.java
+++ src/main/java/it/algos/vaadwiki/application/WikiBoot.java
@@ -64,9 +64,9 @@ public class WikiBoot extends ABoot {
 
     private final static String PROJECT_NAME = "vaadwiki";
 
-    private final static double PROJECT_VERSION = 29;
+    private final static double PROJECT_VERSION = 30;
 
-    private final static LocalDate VERSION_DATE = LocalDate.of(2022, 3, 17);
+    private final static LocalDate VERSION_DATE = LocalDate.of(2022, 3, 19);
 
     private final static String PATH_LOGO = "frontend/images/Emblem-wiki-blue.svg.png";
 
