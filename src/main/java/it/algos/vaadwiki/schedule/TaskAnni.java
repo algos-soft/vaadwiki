529c04e7ffdc24c2c185dea72edc950009e1a749
algos-soft <gac@algos.it>
2022-03-26 12:03:59 +0100

    rimesso task giorni e anni

--- src/main/java/it/algos/vaadwiki/schedule/TaskAnni.java
+++ src/main/java/it/algos/vaadwiki/schedule/TaskAnni.java
@@ -40,7 +40,7 @@ public class TaskAnni extends WikiTask {
      * Esiste il flag 'usaDaemonCroci' per usare o meno questa Task <br>
      * Se la si usa, controlla il flag generale di debug per 'intensificare' l'import <br>
      */
-//    @PostConstruct
+    @PostConstruct
     protected void inizia() {
         super.eaSchedule = EASchedule.oreSei;
         super.usaDaemon = pref.isBool(USA_DAEMON_ANNI_UPLOAD);
